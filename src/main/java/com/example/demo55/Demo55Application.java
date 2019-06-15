f8e487445179c3ac6ed40fd3d300c108df58ca79
ominiscent <omkolte@gmail.com>
2019-06-15 12:52:57 +0530

    hello

---
+++ src/main/java/com/example/demo55/Demo55Application.java
@@ -0,0 +1,13 @@
+package com.example.demo55;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo55Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo55Application.class, args);
+	}
+
+}
